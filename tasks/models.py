0f04c1f9c6b1d6293ee77160e3e28a61e0fff8f7
laskarj <koks.adekvat@gmail.com>
2024-03-06 10:27:53 +0100

    Added Enum Priority for Task, Added model Task, added constraint null=True into Worker's field position

--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,13 @@
+from enum import StrEnum, auto
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
+class Priority(StrEnum):
+    URGENT = auto()
+    HIGH = auto()
+    LOW = auto()
 
 
 class Position(models.Model):
@@ -17,7 +25,8 @@ class Worker(AbstractUser):
         Position,
         on_delete=models.SET_DEFAULT,
         related_name="workers",
-        default=None
+        default=None,
+        null=True,
     )
 
     def __str__(self) -> str:
@@ -29,3 +38,23 @@ class TaskType(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=6,
+        choices=[(priority.value, priority.name) for priority in Priority]
+    )
+    task_type = models.ForeignKey(
+        TaskType,
+        on_delete=models.CASCADE,
+        related_name="tasks"
+    )
+    workers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="tasks"
+    )
