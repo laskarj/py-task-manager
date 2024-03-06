4f941fb4063c81526f2986c905cfd416ffec155e
laskarj <koks.adekvat@gmail.com>
2024-03-06 09:43:57 +0100

    Added Worker model for tasks and set up it for AUTH_USER_MODEL in settings

--- tasks/models.py
+++ tasks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
 class Position(models.Model):
@@ -9,3 +10,15 @@ class Position(models.Model):
 
     class Meta:
         ordering = ["name", ]
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.SET_DEFAULT,
+        related_name="workers",
+        default=None
+    )
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}({self.position})"
